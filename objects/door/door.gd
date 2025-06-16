0b7e93d73e9fe352860e279bb6df9c4679e1ed61
Bryan Miller <bmiller1008@gmail.com>
2025-06-16 19:42:50 -0400

    Pulled in updated assets with textures (thanks again to WhaitJeezus). Arcade machines placed in the scene will now be randomly assigned the available games. Game can be quit by exiting through the double doors.

---
+++ objects/door/door.gd
@@ -0,0 +1,74 @@
+extends Node3D
+
+
+
+# ------------------------------------------------------------------------------
+# Constants
+# ------------------------------------------------------------------------------
+enum State {CLOSED=0, OPENED=1}
+
+const STATE_OPENED_ANGLE : float = deg_to_rad(122.0)
+const STATE_CLOSED_ANGLE : float = 0.0
+const CHANGE_DPS : float = deg_to_rad(90.0)
+
+# ------------------------------------------------------------------------------
+# Export Variables
+# ------------------------------------------------------------------------------
+@export var locked : bool = false
+@export var initial_state : State = State.CLOSED
+
+# ------------------------------------------------------------------------------
+# Variables
+# ------------------------------------------------------------------------------
+var _state : State = State.CLOSED
+var _tween : Tween = null
+
+# ------------------------------------------------------------------------------
+# Onready Variables
+# ------------------------------------------------------------------------------
+@onready var _door: CharacterBody3D = %Door
+
+# ------------------------------------------------------------------------------
+# Override Methods
+# ------------------------------------------------------------------------------
+func _ready() -> void:
+	_state = initial_state
+	match initial_state:
+		State.OPENED:
+			_door.rotation.y = STATE_OPENED_ANGLE
+		State.CLOSED:
+			_door.rotation.y = STATE_CLOSED_ANGLE
+
+# ------------------------------------------------------------------------------
+# Private Methods
+# ------------------------------------------------------------------------------
+func _TweenDoor(angle : float) -> void:
+	if _door == null: return
+	if _tween != null:
+		_tween.kill()
+		_tween = null
+	
+	var diff : float = abs(_door.rotation.y - angle)
+	var duration : float = diff / CHANGE_DPS
+
+	_tween = create_tween()
+	_tween.set_ease(Tween.EASE_IN_OUT)
+	_tween.set_trans(Tween.TRANS_QUAD)
+	_tween.tween_property(_door, "rotation:y", angle, duration)
+	_tween.finished.connect(_on_tween_finished, CONNECT_ONE_SHOT)
+
+# ------------------------------------------------------------------------------
+# Handler Methods
+# ------------------------------------------------------------------------------
+func _on_tween_finished() -> void:
+	_tween = null
+
+func _on_component_interactable_interacted(payload: Dictionary) -> void:
+	match _state:
+		State.OPENED:
+			_state = State.CLOSED
+			_TweenDoor(STATE_CLOSED_ANGLE)
+		State.CLOSED:
+			if not locked:
+				_state = State.OPENED
+				_TweenDoor(STATE_OPENED_ANGLE)
--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,5 +1,9 @@
 extends Node
 
+# ------------------------------------------------------------------------------
+# Constants
+# ------------------------------------------------------------------------------
+const GROUP_ARCADE_MACHINE : StringName = &"ArcadeMachine"
 
 # ------------------------------------------------------------------------------
 # Onready Variables
@@ -13,8 +17,39 @@ extends Node
 func _ready() -> void:
 	GamePool.load_roms()
 	GamePool.game_scan()
+	_PopulateGames.call_deferred()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		if _player != null:
 			_player.active = not _player.active
+
+# ------------------------------------------------------------------------------
+# Private Methods
+# ------------------------------------------------------------------------------
+func _PopulateGames() -> void:
+	var games : Array[StringName] = GamePool.get_games_list()
+	if games.size() <= 0: return
+	
+	var nodes : Array[Node] = get_tree().get_nodes_in_group(GROUP_ARCADE_MACHINE).filter(
+		func (item : Node):
+			return item is ArcadeMachine
+	)
+	if nodes.size() <= 0: return
+	
+	for _i : int in range(games.size()):
+		var gidx : int = randi_range(0, games.size() - 1)
+		var nidx : int = randi_range(0, nodes.size() - 1)
+		
+		var am : ArcadeMachine = nodes[nidx]
+		nodes.remove_at(nidx)
+		
+		am.game_name = games[gidx]
+		games.remove_at(gidx)
+		
+		if nodes.size() <= 0:
+			break
+
+func _on_exit_area_body_entered(body: Node3D) -> void:
+	if _player != null and _player == body:
+		get_tree().quit()
