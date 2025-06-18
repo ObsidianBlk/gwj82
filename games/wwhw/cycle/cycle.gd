d0a0f3b3736aa0cd018da26b435daabbdec6adad
Bryan Miller <bmiller1008@gmail.com>
2025-06-18 14:10:30 -0400

    Started work on a new game, 'Wrong Way Highway' (incomplete).

---
+++ games/wwhw/cycle/cycle.gd
@@ -0,0 +1,126 @@
+extends Node2D
+
+
+# ------------------------------------------------------------------------------
+# Signals
+# ------------------------------------------------------------------------------
+signal crashed()
+
+# ------------------------------------------------------------------------------
+# Constants
+# ------------------------------------------------------------------------------
+const BOUNDS : Vector2 = Vector2(-120, 120)
+const LANES : Array[float] = [-100.0, 0.0, 100.0]
+
+const RIDER_LEFT : int = 1
+const RIDER_CENTER : int = 0
+const RIDER_RIGHT : int = 2
+
+const SPEED : float = 160.0
+
+const BOUNCE_DELAY : float = 0.4
+
+# ------------------------------------------------------------------------------
+# Variables
+# ------------------------------------------------------------------------------
+var _lane_idx : int = 1
+var _tween : Tween = null
+
+# ------------------------------------------------------------------------------
+# Onready Variables
+# ------------------------------------------------------------------------------
+@onready var _rider: Sprite2D = %Rider
+
+
+# ------------------------------------------------------------------------------
+# Private Methods
+# ------------------------------------------------------------------------------
+func _UpdateRider(frame : int) -> void:
+	if _rider == null: return
+	var frame_count : int = _rider.hframes * _rider.vframes
+	if frame >= 0 and frame < frame_count:
+		_rider.frame = frame
+
+func _TweenBounce(edge : float) -> void:
+	if _tween != null: return
+
+	var from : Vector2 = position
+	var to : Vector2 = Vector2(edge, position.y)
+	var dist = from.distance_to(to)
+	var duration : float = dist / SPEED
+	
+	var rider_in_frame : int = RIDER_LEFT
+	var rider_out_frame : int = RIDER_RIGHT
+	if from.x < to.x:
+		rider_in_frame = RIDER_RIGHT
+		rider_out_frame = RIDER_LEFT
+	
+	_UpdateRider(rider_in_frame)
+	
+	_tween = create_tween()
+	_tween.set_ease(Tween.EASE_IN_OUT)
+	_tween.set_trans(Tween.TRANS_SINE)
+	
+	_tween.set_parallel(false)
+	_tween.tween_property(self, "position", to, duration)
+	_tween.tween_callback(_UpdateRider.bind(RIDER_CENTER))
+	_tween.tween_interval(BOUNCE_DELAY)
+	_tween.tween_callback(_UpdateRider.bind(rider_out_frame))
+	_tween.tween_property(self, "position", from, duration)
+	_tween.tween_callback(_UpdateRider.bind(RIDER_CENTER))
+	_tween.finished.connect(_on_tween_finished, CONNECT_ONE_SHOT)
+
+
+func _TweenTo(dst : float) -> void:
+	if _tween != null: return
+	
+	var to : Vector2 = Vector2(dst, position.y)
+	var dist = position.distance_to(to)
+	var duration : float = dist / SPEED
+	
+	if position.x < dst:
+		_UpdateRider(RIDER_RIGHT)
+	else:
+		_UpdateRider(RIDER_LEFT)
+	
+	_tween = create_tween()
+	_tween.set_ease(Tween.EASE_IN_OUT)
+	_tween.set_trans(Tween.TRANS_SINE)
+	
+	_tween.set_parallel(false)
+	_tween.tween_property(self, "position", to, duration)
+	_tween.tween_callback(_UpdateRider.bind(RIDER_CENTER))
+	_tween.finished.connect(_on_tween_finished, CONNECT_ONE_SHOT)
+
+func _Die() -> void:
+	crashed.emit()
+	# Spawn an explosion?
+	queue_free()
+
+# ------------------------------------------------------------------------------
+# Public Methods
+# ------------------------------------------------------------------------------
+func move_left() -> void:
+	_lane_idx -= 1
+	if _lane_idx < 0:
+		_lane_idx = 0
+		_TweenBounce(BOUNDS.x)
+	else:
+		_TweenTo(LANES[_lane_idx])
+
+func move_right() -> void:
+	_lane_idx += 1
+	if _lane_idx >= LANES.size():
+		_lane_idx = LANES.size() - 1
+		_TweenBounce(BOUNDS.y)
+	else:
+		_TweenTo(LANES[_lane_idx])
+
+# ------------------------------------------------------------------------------
+# Handler Methods
+# ------------------------------------------------------------------------------
+func _on_tween_finished() -> void:
+	_tween = null
+
+func _on_hit_area_area_entered(area: Area2D) -> void:
+	_Die.call_deferred()
---
+++ games/wwhw/wwhw.gd
@@ -0,0 +1,102 @@
+extends ArcadeGame
+
+
+# ------------------------------------------------------------------------------
+# Constants
+# ------------------------------------------------------------------------------
+const BG_SPEED : float = 120.0
+const BG_BOUNDS : Vector2 = Vector2(-136, 152)
+
+const MILES_TO_MULTIPLIER : int = 4
+const SEGS_TO_SCORE : int = 24
+const SCORE_DISTANCE : int = 1
+const SCORE_CRASH : int = -5
+
+const CAR_SCENES : Array[PackedScene] = [
+	preload("res://games/wwhw/objects/car_a/car_a.tscn"),
+	preload("res://games/wwhw/objects/car_b/car_b.tscn"),
+	preload("res://games/wwhw/objects/car_c/car_c.tscn"),
+]
+
+# ------------------------------------------------------------------------------
+# Variables
+# ------------------------------------------------------------------------------
+var _cycle : WeakRef = weakref(null)
+var _segment : int = 0
+var _miles : int = 0
+var _multiplier : int = 1
+
+# ------------------------------------------------------------------------------
+# Onready Variables
+# ------------------------------------------------------------------------------
+@onready var _backdrop: Node2D = %Backdrop
+@onready var _lane_center: Node2D = %LaneCenter
+@onready var _lane_left: Node2D = %LaneLeft
+@onready var _lane_right: Node2D = %LaneRight
+
+
+# ------------------------------------------------------------------------------
+# Override Methods
+# ------------------------------------------------------------------------------
+func _unhandled_input(event: InputEvent) -> void:
+	if not active : return
+	
+	var cycle : Node2D = _GetPlayerCycle()
+	if cycle == null: return
+	
+	if event.is_action_pressed("game_left"):
+		cycle.move_left()
+	elif event.is_action_pressed("game_right"):
+		cycle.move_right()
+
+func _process(delta: float) -> void:
+	_UpdateBackground(delta)
+
+# ------------------------------------------------------------------------------
+# Private Methods
+# ------------------------------------------------------------------------------
+func _UpdateBackground(delta : float) -> void:
+	if _backdrop == null: return
+	var amount : float = BG_SPEED * delta
+	for child in _backdrop.get_children():
+		if child is Sprite2D:
+			child.position.y += amount
+			if child.position.y >= BG_BOUNDS.y:
+				child.position.y -= BG_BOUNDS.y - BG_BOUNDS.x
+				_UpdateSegment()
+
+func _UpdateSegment() -> void:
+	if not active: return
+	_segment += 1
+	if _segment > 0 and _segment % SEGS_TO_SCORE == 0:
+		update_score(SCORE_DISTANCE * _multiplier)
+		_miles += 1
+		if _miles > 0 and _miles % MILES_TO_MULTIPLIER == 0:
+			_multiplier += 1
+
+func _GetPlayerCycle() -> Node2D:
+	var cycle : Node2D = _cycle.get_ref()
+	if cycle == null:
+		for child : Node in get_children():
+			if child.has_method(&"move_left") and child.has_method(&"move_right"):
+				cycle = child
+		if cycle != null:
+			_cycle = weakref(cycle)
+	return cycle
+
+func _LaneHasCar(lane : Node2D) -> bool:
+	if lane == null: return false
+	for child : Node in lane.get_children():
+		if child is WWHWCar:
+			return true
+	return false
+
+func _SpawnHazard() -> void:
+	var has_cars : Array[bool] = [
+		_LaneHasCar(_lane_left),
+		_LaneHasCar(_lane_center),
+		_LaneHasCar(_lane_right)
+	]
+	
+	if has_cars.filter(func(oc : bool): return oc).size() <= 1:
+		pass
